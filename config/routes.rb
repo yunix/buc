906978aa93c51b6fe2c9073fcc219bbf96c66ba3
yunix <pcarrillo@adventist.org.uk>
2014-09-01 10:47:33 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Extension::Routes.draw do |map|
 
 # your extension routes go here
-#map.namespace(:admin) do |admin|
-	#admin.resource  :linktocontents, :collection => { :load => :get }
-#end
-map.resource :linktocontents, :collection=>{:link=>:get}
+
 end
