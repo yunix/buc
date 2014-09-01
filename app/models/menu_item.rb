5d758bfa9e6b4d97d5e1749ce8b98bed4ce3a751
yunix <pcarrillo@adventist.org.uk>
2014-09-01 10:34:39 +0100

    Update menu_item.rb

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,8 +1,5 @@
 class MenuItem < ActiveRecord::Base
   belongs_to :site
-  
-    
- validates_presence_of :text
- validates_presence_of :url
-
+  #validates_presence_of :text
+  #validates_presence_of :url
 end
